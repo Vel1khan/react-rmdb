501a9cb3f01f3c8138cbbc58ec7520d070dae888
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2021-11-06 17:14:05 +0100

    Worked on Homepage banner (WIP)

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 //Components
-
+import HeroImage from './HeroImage';
 // Hook
 import {useHomeFetch} from '../hooks/useHomeFetch';
 
@@ -16,7 +16,20 @@ const Home = () => {
     const {state, loading, error} = useHomeFetch();
     console.log(state);
 
-    return <div>Home Page</div>
-}
+    return (
+        //Fragment, returning a single element without creating a div
+        <>
+            {state.results[0] ?
+            <HeroImage
+                //Template literal
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            />
+            : null
+            }
+        </>
+    );
+};
 
 export default Home;
