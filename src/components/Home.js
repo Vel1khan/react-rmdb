0f30f885b13e14be3fa40fb1705fc854afd64970
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2021-11-06 15:51:08 +0100

    Added ability to fetch movies using TMDB API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+//API
+import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 //Components
@@ -12,6 +14,32 @@ const Home = () => {
     const [state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm,page);
+
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+
+        } catch (e) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    // useEffect dependency set as [] means it will only run once
+    useEffect(() => {
+        fetchMovies(1);
+    }, []);
+
+    console.log(state);
 
     return <div>Home Page</div>
 }
