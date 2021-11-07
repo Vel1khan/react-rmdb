d4bceb75a9d6bc8c6dd68732ff4635ec5b245ce0
Vel1khan <35954057+Vel1khan@users.noreply.github.com>
2021-11-07 21:29:57 +0100

    Sync changes up to MovieInfo Component video in tutorial

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -9,9 +9,11 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
@@ -33,9 +35,20 @@ export const useHomeFetch = () => {
     };
 
     // useEffect dependency set as [] means it will only run once
+    //Initial and search render
     useEffect(() => {
-        fetchMovies(1);
-    }, []);
+        setState(initialState);
+        fetchMovies(1, searchTerm);
+    }, [searchTerm]);
 
-    return { state, loading, error};
+    // Load More
+    useEffect (() => {
+        if (!isLoadingMore) return;
+
+        fetchMovies(state.page + 1, searchTerm);
+        setIsLoadingMore(false);
+
+    },[isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore};
 }
